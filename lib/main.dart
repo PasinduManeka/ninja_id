600040479c90392248555f646c20b31e8ec5e6de
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-04-18 07:08:59 +0530

    Added the email avatar

--- lib/main.dart
+++ lib/main.dart
@@ -47,11 +47,39 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height:30.0,),
             Text(
-               'Name',
+               'Current Ninja level',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
+            ),
+            SizedBox(height:10.0,),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color:Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'pasinduherath18@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
             )
           ],
         ),
